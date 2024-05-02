97f2d7ca81eb0c622d43fbdc944c9e1beedf164d
justpeterpan <peter.busch@posteo.net>
2024-05-02 21:39:04 +0200

    fix: try simple comment

--- server/api/commit.ts
+++ server/api/commit.ts
@@ -7,13 +7,13 @@ export default defineEventHandler(async (event) => {
 
   const projectId = project.id
   const lastCommitId = object_attributes.last_commit.id
-  const mergeRequestId = object_attributes.id
+  const mergeRequestId = object_attributes.iid
 
   const data = await useGitlabApi(event)<unknown[]>(
     `/${projectId}/repository/commits/${lastCommitId}/diff`
   )
 
-  const bodyData = JSON.stringify({ body: data + '\n' + 'LGTM' })
+  const bodyData = JSON.stringify({ body: 'Hello World!' + data })
 
   const response = await useGitlabApi(event)<unknown[]>(
     `/${projectId}/merge_requests/${mergeRequestId}/discussions`,
