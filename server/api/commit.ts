ecf2beaa00612b123ebed807ed500f26d227cc09
justpeterpan <peter.busch@posteo.net>
2024-05-03 01:00:50 +0200

    refactor: move code around

--- server/api/commit.ts
+++ server/api/commit.ts
@@ -7,35 +7,17 @@ export default defineEventHandler(async (event) => {
 
   const projectId = project.id
   const lastCommitId = object_attributes.last_commit.id
-  const mergeRequestId = object_attributes.iid
 
-  setImmediate(async () => {
-    const data = await useGitlabApi(event)<unknown[]>(
-      `/${projectId}/repository/commits/${lastCommitId}/diff`
-    )
+  const data = await useGitlabApi(event)<unknown[]>(
+    `/${projectId}/repository/commits/${lastCommitId}/diff`
+  )
 
-    const message = await sendMessages(event, '@cf/meta/llama-3-8b-instruct', [
-      {
-        role: 'system',
-        content: 'You are a kind, helpful senior frontend developer.',
-      },
-      { role: 'user', content: JSON.stringify(data) },
-      {
-        role: 'system',
-        content:
-          "Analyze the user's code and provide feedback. Remember to be kind and helpful. Give them a chance to learn and grow. But also be honest and direct. They need to know what they did wrong and how to fix it. But also what they did right. They need to know what they did well and how to do more of it.",
-      },
-    ])
-
-    const bodyData = JSON.stringify({ body: JSON.stringify(message) })
-
-    await useGitlabApi(event)<unknown[]>(
-      `/${projectId}/merge_requests/${mergeRequestId}/discussions`,
-      {
-        method: 'POST',
-        body: bodyData,
-      }
-    )
+  $fetch('/api/webhook', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ project, object_attributes, data }),
   })
 
   return { status: 'Processing started' }
--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -1,9 +1,30 @@
 import { sendMessages } from '../utils/cf-api'
 
 export default defineEventHandler(async (event) => {
-  const data = await readBody(event)
+  const { project, object_attributes, data } = await readBody(event)
+  const projectId = project.id
+  const mergeRequestId = object_attributes.iid
 
-  return sendMessages(event, '@cf/meta/llama-3-8b-instruct', [
-    { role: 'user', content: 'What is the origin of the phrase Hello, World?' },
+  const message = await sendMessages(event, '@cf/meta/llama-3-8b-instruct', [
+    {
+      role: 'system',
+      content: 'You are a kind, helpful senior frontend developer.',
+    },
+    { role: 'user', content: JSON.stringify(data) },
+    {
+      role: 'system',
+      content:
+        "Analyze the user's code and provide feedback. Remember to be kind and helpful. Give them a chance to learn and grow. But also be honest and direct. They need to know what they did wrong and how to fix it. But also what they did right. They need to know what they did well and how to do more of it.",
+    },
   ])
+
+  const bodyData = JSON.stringify({ body: JSON.stringify(message) })
+
+  await useGitlabApi(event)<unknown[]>(
+    `/${projectId}/merge_requests/${mergeRequestId}/discussions`,
+    {
+      method: 'POST',
+      body: bodyData,
+    }
+  )
 })
