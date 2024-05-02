0d1010733501902e2c5e0ff7c184408bb525bfec
justpeterpan <peter.busch@posteo.net>
2024-05-02 03:32:50 +0200

    debug: return readbody event data

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -4,7 +4,8 @@ export default defineEventHandler(async (event) => {
   const data = await readBody(event)
   console.log(data)
 
-  return sendMessages(event, '@cf/meta/llama-3-8b-instruct', [
-    { role: 'user', content: 'What is the origin of the phrase Hello, World?' },
-  ])
+  return data
+  // return sendMessages(event, '@cf/meta/llama-3-8b-instruct', [
+  //   { role: 'user', content: 'What is the origin of the phrase Hello, World?' },
+  // ])
 })
