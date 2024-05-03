86c9314c2209feb56e464ab35d813b31d5a612ce
justpeterpan <peter.busch@posteo.net>
2024-05-03 20:16:42 +0200

    chore(merja): provide correct nuxt.config.ts

--- apps/merja/nuxt.config.ts
+++ apps/merja/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  experimental: {
+    noVueServer: true,
+  },
+  routeRules: {
+    '/': { prerender: true },
+  },
+  modules: ['nitro-cloudflare-dev'],
+  runtimeConfig: {
+    cloudflare: {
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+      token: process.env.CLOUDFLARE_API_TOKEN,
+    },
+    gitlab: {
+      apiToken: process.env.GITLAB_API_TOKEN,
+    },
+  },
+  nitro: {
+    preset: 'cloudflare-pages',
+  },
 })
