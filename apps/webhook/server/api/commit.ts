65396d568bb4904f938f275c6d66f2c11c8b61a1
justpeterpan <peter.busch@posteo.net>
2024-05-04 11:04:01 +0200

    feat: try out message queue

--- apps/webhook/server/api/commit.ts
+++ apps/webhook/server/api/commit.ts
@@ -1,13 +1,6 @@
-import { H3Event } from 'h3'
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig(event)
   const { project, object_attributes, object_kind } = await readBody(event)
   // const { projectId, lastCommitId, mergeRequestId } = await readBody(event)
-
-  await event.context.cloudflare.env.kv.put(
-    `${new Date()} start merger`,
-    'start'
-  )
 
   if (object_kind !== 'merge_request') {
     return
@@ -16,41 +9,14 @@ export default defineEventHandler(async (event) => {
   const projectId = project.id
   const lastCommitId = object_attributes.last_commit.id
   const mergeRequestId = object_attributes.iid
+  const timestamp = Math.floor(Date.now() / 1000)
 
-  const bodyData = JSON.stringify({ projectId, mergeRequestId, lastCommitId })
-  await event.context.cloudflare.env.kv.put(
-    `${new Date()} data merger`,
-    bodyData
+  await event.context.cloudflare.env.kv.put('timestamp_projectId', projectId)
+  await event.context.cloudflare.env.kv.put('timestamp_lastCommitId', lastCommitId)
+  await event.context.cloudflare.env.kv.put('timestamp_mergeRequestId', mergeRequestId)
+  await event.context.cloudflare.env.queue.send(timestamp)
+
+  await event.context.cloudflare.env.queue.event.node.res.end(
+    'Webhook received and being processed.'
   )
-
-  event.node.res.end('Webhook received and being processed.')
-
-  await event.context.cloudflare.env.kv.put(
-    `${new Date()} fetch`,
-    'calling fetch'
-  )
-
-  callUrl(config.merja.baseUrl, bodyData, event)
-
-  // $fetch(config.merja.baseUrl, {
-  //   method: 'POST',
-  //   body: bodyData,
-  // }).catch((err) => {
-  //   event.context.cloudflare.env.kv.put(
-  //     `${new Date()} error`,
-  //     JSON.stringify(err)
-  //   )
-  // })
-
-  // return
 })
-
-function callUrl(url: string, data: string, event: H3Event) {
-  event.context.cloudflare.env.kv.put(
-    `${new Date()} inside fetch`,
-    `calling fetch ${url}, ${data}`
-  )
-  $fetch(url, {
-    body: data,
-  })
-}
