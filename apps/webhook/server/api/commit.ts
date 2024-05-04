a7144e7bd6254d98411dcc1ea63ae3345658290d
justpeterpan <peter.busch@posteo.net>
2024-05-04 10:00:24 +0200

    feat: add kv binding

--- apps/webhook/server/api/commit.ts
+++ apps/webhook/server/api/commit.ts
@@ -3,13 +3,15 @@ export default defineEventHandler(async (event) => {
   const { project, object_attributes, object_kind } = await readBody(event)
   // const { projectId, lastCommitId, mergeRequestId } = await readBody(event)
 
-  if (event.context.cloudflare.env.r2) {
-    console.log('puuuuuuuuush')
-    await event.context.cloudflare.env.r2.put(
-      'start',
-      new Blob(['start'], { type: 'text/plain' })
-    )
-  }
+  // if (event.context.cloudflare.env.r2) {
+  //   console.log('puuuuuuuuush')
+  //   await event.context.cloudflare.env.r2.put(
+  //     'start',
+  //     new Blob(['start'], { type: 'text/plain' })
+  //   )
+  // }
+  console.log(event.context.cloudflare.env.kv)
+  await event.context.cloudflare.env.kv.put('start', 'start')
 
   if (object_kind !== 'merge_request') {
     return
@@ -19,19 +21,16 @@ export default defineEventHandler(async (event) => {
   const lastCommitId = object_attributes.last_commit.id
   const mergeRequestId = object_attributes.iid
 
-  const varsData = `projectId: ${projectId}, mergeRequestId: ${mergeRequestId}, lastCommitId: ${lastCommitId}`
-  await event.context.cloudflare.env.r2.put(
-    'vars',
-    new Blob([varsData], { type: 'text/plain' })
-  )
+  // const varsData = `projectId: ${projectId}, mergeRequestId: ${mergeRequestId}, lastCommitId: ${lastCommitId}`
+  // await event.context.cloudflare.env.r2.put(
+  //   'vars',
+  //   new Blob([varsData], { type: 'text/plain' })
+  // )
 
   const bodyData = JSON.stringify({ projectId, mergeRequestId, lastCommitId })
-
-  await event.context.cloudflare.env.r2.put('bodyData', bodyData)
+  await event.context.cloudflare.env.kv.put('bodyData', bodyData)
 
   event.node.res.end('Webhook received and being processed.')
-
-  await event.context.cloudflare.env.r2.put('bodyData', 'running fetch')
 
   $fetch(config.merja.baseUrl, {
     method: 'POST',
