ee5ecbf3419de72010c4a8b0792dbe63d1138e20
justpeterpan <peter.busch@posteo.net>
2024-05-04 11:41:34 +0200

    chore: clean up code and remove kv binding

--- apps/webhook/server/api/commit.ts
+++ apps/webhook/server/api/commit.ts
@@ -1,22 +1,14 @@
 export default defineEventHandler(async (event) => {
   const { project, object_attributes, object_kind } = await readBody(event)
-  // const { projectId, lastCommitId, mergeRequestId } = await readBody(event)
 
-  if (object_kind !== 'merge_request') {
-    return
-  }
+  if (object_kind !== 'merge_request') return
 
   const projectId = project.id
   const lastCommitId = object_attributes.last_commit.id
   const mergeRequestId = object_attributes.iid
-  const timestamp = Math.floor(Date.now() / 1000)
 
-  await event.context.cloudflare.env.kv.put(`${timestamp}_projectId`, projectId)
-  await event.context.cloudflare.env.kv.put(`${timestamp}_lastCommitId`, lastCommitId)
-  await event.context.cloudflare.env.kv.put(`${timestamp}_mergeRequestId`, mergeRequestId)
-  await event.context.cloudflare.env.queue.send(timestamp)
+  const data = JSON.stringify({pid: projectId, cid: lastCommitId, mid: mergeRequestId})
 
-  event.node.res.end(
-    'Webhook received and being processed.'
-  )
+  await event.context.cloudflare.env.queue.send(data)
+  event.node.res.end('Webhook received and being processed.')
 })
