895a241d1640af77730d68fa7127d0121d207747
justpeterpan <peter.busch@posteo.net>
2024-05-04 09:26:26 +0200

    feat: add r2 binding and logging

--- apps/webhook/server/api/commit.ts
+++ apps/webhook/server/api/commit.ts
@@ -3,6 +3,14 @@ export default defineEventHandler(async (event) => {
   const { project, object_attributes, object_kind } = await readBody(event)
   // const { projectId, lastCommitId, mergeRequestId } = await readBody(event)
 
+  if (event.context.cloudflare.env.r2) {
+    console.log('puuuuuuuuush')
+    await event.context.cloudflare.env.r2.put(
+      'start',
+      'Webhook received and being processed.'
+    )
+  }
+
   if (object_kind !== 'merge_request') {
     return
   }
@@ -11,9 +19,18 @@ export default defineEventHandler(async (event) => {
   const lastCommitId = object_attributes.last_commit.id
   const mergeRequestId = object_attributes.iid
 
+  await event.context.cloudflare.env.r2.put(
+    'vars',
+    `projectId: ${projectId}, mergeRequestId: ${mergeRequestId}, lastCommitId: ${lastCommitId}`
+  )
+
   const bodyData = JSON.stringify({ projectId, mergeRequestId, lastCommitId })
 
+  await event.context.cloudflare.env.r2.put('bodyData', bodyData)
+
   event.node.res.end('Webhook received and being processed.')
+
+  await event.context.cloudflare.env.r2.put('bodyData', 'running fetch')
 
   $fetch(config.merja.baseUrl, {
     method: 'POST',
